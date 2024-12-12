6ff75391ee59793beab4dec651c59b2e3593e7d5
saurabhzenmind <saurabhawani@gmail.com>
2024-12-12 15:56:19 +0530

    Update stockavg2.js

--- stockavg2.js
+++ stockavg2.js
@@ -7,17 +7,20 @@ function getAverage(){
        var res1 = (unit1 * price1) + (unit2 * price2);
        var res2 = +unit1 + +unit2;
        var result = res1 / res2;
-       var totcost = price1*price2;
+       var totres=result.toFixed(2);
        var currinv=unit1*price1;
        var newinv=unit2*price2;
+       var totinv= currinv+newinv;
        var netchng1=((price2-price1)/price1)*100;
+       var roundchng1=netchng1.toFixed(2);
        var netchng2=((price2-result)/result)*100;
+       var roundchng2=netchng2.toFixed(2);
        document.getElementById("result").innerHTML="Total units:&nbsp;<bold>"+ res2 +
-       "</bold></br></br>Average Price:&nbsp;<bold>"+ result+"rs"+
-       "</bold></br></br>Total Cost:&nbsp;<bold>" +totcost+"rs"+
        "</bold></br></br>Current Investment:&nbsp<bold>"+currinv+"rs"+
        "</bold></br></br>New Investment:&nbsp;<bold>"+newinv+"rs"+
-       "</bold></br></br>Net Change:&nbsp;<bold>"+netchng1+"%"+
-       "</bold></br></br>New Net Change Will Be:&nbsp;<bold>"+netchng2+"%"+"</bold>";
+       "</bold></br></br>Total Investment:&nbsp;<bold>" +totinv+"rs"+
+       "</bold></br></br>Average Price:&nbsp;<bold>"+ totres +"rs"+
+       "</bold></br></br>Net Change:&nbsp;<bold>"+roundchng1+"%"+
+       "</bold></br></br>New Net Change Will Be:&nbsp;<bold>"+roundchng2+"%"+"</bold>";
  
  }
