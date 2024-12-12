9d692ce3b25ef1f67b8dc4f17d5a3c09a3f03a90
saurabhzenmind <saurabhawani@gmail.com>
2024-12-12 15:55:32 +0530

    Update stockavg1.js

--- stockavg1.js
+++ stockavg1.js
@@ -7,13 +7,13 @@ function getAverage(){
      var res1 = (unit1 * price1) + (unit2 * price2);
      var res2 = +unit1 + +unit2;
      var result = res1 / res2;
-     var totcost = price1*price2;
+     var totres=result.toFixed(2);
      var currinv=unit1*price1;
      var newinv=unit2*price2;
+     var totinv= currinv+newinv;
      var netchng1=((price2-price1)/price1)*100;
      var netchng2=((price2-result)/result)*100;
-     document.getElementById("result").innerHTML = "Total units:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <bold>"
-     + res2 + "</bold><br/><br/>Average Price: &nbsp;&nbsp;&nbsp;<bold>" + result + "</bold></bold></br></br>Total Cost:&nbsp;<bold>" +totcost+"</bold>";
- 
-
+     document.getElementById("result").innerHTML = "Total units:&nbsp;&nbsp; <bold>"+ res2 +
+      "</bold><br/><br/>Average Price: &nbsp;&nbsp;&nbsp;<bold>" + totres + 
+      "</bold></bold></br></br>Total Cost:&nbsp;<bold>" +totinv+"rs"+"</bold>";
 }
